40ee78e01664825333a4da1ee0a46054c9bcd4ce
shs395 <shs395@ajou.ac.kr>
2022-09-17 13:29:59 +0900

    programmers: 멀리 뛰기

---
+++ programmers/Lv2/멀리_뛰기.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = 0
+    dp = [0 for _ in range(n + 1)]
+    dp[1] = 1
+    if n >= 2:
+        dp[2] = 2
+    if n >= 3:
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        
+    return dp[n] % 1234567
