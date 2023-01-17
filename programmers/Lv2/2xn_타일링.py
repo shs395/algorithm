2cbbe703c0be13ea60e0e33760aebca5cf82aa36
shs395 <shs395@ajou.ac.kr>
2023-01-17 23:39:37 +0900

    programmers: 2xn 타일링

---
+++ programmers/Lv2/2xn_타일링.py
@@ -0,0 +1,11 @@
+def solution(n):
+    dp = [0] * (n + 1)
+    dp[0] = 0
+    dp[1] = 1
+    if n >= 2:
+        dp[2] = 2
+    
+    if n >= 3:
+        for i in range(3, n + 1):
+            dp[i] = (dp[i - 2] + dp[i - 1]) % 1000000007
+    return dp[n]
