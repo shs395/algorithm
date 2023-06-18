3707f1039115cbb6857bf7494d4ac79d0a62d275
shs395 <shs395@ajou.ac.kr>
2023-06-18 19:00:07 +0900

    baekjoon: 1026 - 보물

---
+++ baekjoon/1026.py
@@ -0,0 +1,12 @@
+# 1026 - 보물
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+answer = 0
+A.sort()
+B.sort(reverse=True)
+
+for i in range(N):
+    answer += A[i] * B[i]
+
+print(answer)
